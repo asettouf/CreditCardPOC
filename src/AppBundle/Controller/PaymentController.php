90bcf6c31f4886471ad2d2f2e7a74ba544b41767
asettouf <adonis.settouf@gmail.com>
2015-12-03 00:42:43 +0100

    Fixing deprecation issue on getRequest method

--- src/AppBundle/Controller/PaymentController.php
+++ src/AppBundle/Controller/PaymentController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class PaymentController extends Controller{
   /**
@@ -16,7 +16,6 @@ class PaymentController extends Controller{
     $html = $this -> renderView("/payment/index.html.twig");
     return new Response($html);
   }
-
 
   /**
   * @Route("/payment/visa", name="paymentVisa")
@@ -31,8 +30,8 @@ class PaymentController extends Controller{
    * @Route("/payment/visa", name="paymentVisaSubmitted")
    * @Method("POST")
    */
-  public function onCardSubmission(){
-    $amount = $this -> getRequest() -> get("amount");
+  public function onCardSubmission(Request $request){
+    $amount = $request -> get("amount");
     $html = $this -> renderView("/payment/visaFail.html.twig",
       array("amount" => $amount));
     return new Response($html);
