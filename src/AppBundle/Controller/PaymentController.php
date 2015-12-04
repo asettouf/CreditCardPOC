2252b2b3aff211fc50c4c422b12fdabc26ba2b06
asettouf <adonis.settouf@gmail.com>
2015-12-04 14:14:38 +0100

    Adding the model class handling the payment

--- src/AppBundle/Controller/PaymentController.php
+++ src/AppBundle/Controller/PaymentController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\PaymentManagerEntity;
 
 class PaymentController extends Controller{
   /**
